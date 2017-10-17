900eaa614bc08f9bfe8a7e73f4f31adc292eaa2d
maomao9003 <zhangkai-9003@163.com>
2017-10-17 16:15:57 +0800

    Add comments.

--- Algorithms/Algorithms/A-02-1-InsertionSort/InsertionSort.cpp
+++ Algorithms/Algorithms/A-02-1-InsertionSort/InsertionSort.cpp
@@ -31,10 +31,12 @@ void InsertionSort::m_BuildInput()
 
 void InsertionSort::m_Execute()
 {
+    //When i is x, means the 0~x-1 of array is sorted.
     for(int i=1;i<m_size;i++)
     {
         int current = m_array[i];
         int j=i-1;
+        //Copy array[j] -> array[j+1], until find the place to insert array[i]
         for(;j>=0 && m_array[j]>current;j--)
         {
             m_array[j+1] = m_array[j];
