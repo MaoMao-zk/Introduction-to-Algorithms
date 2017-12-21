a143d09582f553a3884a91ff86d576c866f3d9be
sharmylu <sharmylu@163.com>
2017-12-21 17:03:51 +0800

    sharmylu:fix partly code standars issue #2,and cpplint add filter items

--- Code/Common/Algorithms.cpp
+++ Code/Common/Algorithms.cpp
@@ -14,11 +14,11 @@ void Algorithms::Run()
     m_BuildInput();
 
     timeval tv1, tv2;
-    gettimeofday(&tv1,NULL);
+    gettimeofday(&tv1, NULL);
 
     m_Execute();
 
-    gettimeofday(&tv2,NULL);
+    gettimeofday(&tv2, NULL);
     long long used_time = (tv2.tv_sec*1000*1000 + tv2.tv_usec) - (tv1.tv_sec*1000*1000 + tv1.tv_usec);
     long long seconds = used_time / (1000*1000);
     used_time = used_time % (1000*1000);
